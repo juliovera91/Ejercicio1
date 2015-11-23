b9d0cb75f2171bdd84fe33dbe81a3e72c32b2f1a
Julio Cesar Vera Segura <JulioVera@macbook-pro-de-julio.local>
2015-11-22 21:15:05 -0600

    Evaluacion

---
+++ Ejercicio1.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+var i = 1 ... 100
+for a in i{
+    if a % 2  == 0 {  // SE VALIDA SI EL NÙMERO ES PAR
+        print("#\(a) par!!!")
+    }else{ // SI NO ES IMPAR
+        print("#\(a) impar!!!")
+    }
+    
+    if a % 5  == 0 {  // SE VALIDA SI EL NÙMERO ES DIVISIBLE ENTRE 5
+        print("#\(a) Bingo!!!")
+    }
+    
+    if a >= 30  && a <= 40{ // SE VALIDA SI SE ENCUENTRA DENTRO DEL RANGO DE 30 A 40
+        print("#\(a) Viva Swift!!!")
+    }
+}
+
+
